845b626097bdfbb3c18e74b74d4cbe4e459e2084
Sernok <zaitsevsergey12@yandex.ru>
2025-03-16 13:59:48 +0500

    AddMap

---
+++ WebApplication1/Pages/Map.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class MapModel : PageModel
+    {
+        private readonly ILogger<MapModel> _logger;
+
+        public MapModel(ILogger<MapModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
